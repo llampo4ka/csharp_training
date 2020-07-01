e467e77d1dc41ef852c31473a8546094c468787d
llampo4ka <llampo4ka@gmail.com>
2020-07-01 11:56:22 +0300

    ContactInformationFromContactPageTest - Ex/12

--- addressbook-web-tests/addressbook-web-tests/Tests/ContactInformationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/ContactInformationTests.cs
@@ -25,32 +25,60 @@ namespace addressbook_web_tests
         public void ContactInformationFromContactPageTest()
         {
             ContactData fromForm = app.Contacts.GetContactInformationFromForm(0);
-            string dd = fromForm.FirstName + " " + fromForm.LastName + fromForm.Address;
+            string dd = fromForm.FirstName + " " + fromForm.LastName;
 
             if (fromForm.MiddleName != "")
             {
-                dd = fromForm.FirstName + " " + fromForm.MiddleName + " " + fromForm.LastName + fromForm.Address;
+                dd = fromForm.FirstName + " " + fromForm.MiddleName + " " + fromForm.LastName;
             }
-                        
+
+            if (fromForm.Address != "")
+            {
+                dd += "\r\n" + fromForm.Address;
+            }
+
+            dd += "\r\n\r\n"; 
+
+
             if (fromForm.HomePhone != "")
             {
-                dd = dd + "H: " + fromForm.HomePhone;
+                dd = dd + "H: " + fromForm.HomePhone + "\r\n";
             }
             if (fromForm.MobilePhone != "")
             {
-                dd = dd + "M: " + fromForm.MobilePhone;
+                dd = dd + "M: " + fromForm.MobilePhone + "\r\n";
             }
             if (fromForm.WorkPhone != "")
             {
-                dd = dd + "W: " + fromForm.WorkPhone;
+                dd = dd + "W: " + fromForm.WorkPhone + "\r\n";
             }
-           
-            dd += fromForm.Email + fromForm.Email2 + fromForm.Email3;
 
-            System.Console.Out.Write(dd);
+            if (fromForm.HomePhone != "" || fromForm.MobilePhone != "" || fromForm.WorkPhone != "")
+            {
+                dd += "\r\n";
+            }
+
+            if (fromForm.Email != "")
+            {
+                dd += fromForm.Email + "\r\n";
+            }
+
+            if (fromForm.Email2 != "")
+            {
+                dd += fromForm.Email2 + "\r\n";
+            }
+
+            if (fromForm.Email3 != "")
+            {
+                dd += fromForm.Email3 + "\r\n";
+            }
+
+            dd = dd.Trim();
+
+            //System.Console.Out.Write(dd);
             string fromContPage = app.Contacts.GetContactInformationFromPage(0);
-            fromContPage = Regex.Replace(fromContPage, "[\r\n]", "");
-            System.Console.Out.Write(fromContPage);
+            
+           // System.Console.Out.Write(fromContPage);
             Assert.AreEqual(fromContPage, dd);
         }
     }
